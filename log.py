5d78931c95baf2fa6dfbcc5dd34b9bc102d9090c
Abbas <abbazs@gmail.com>
2019-04-06 13:36:45 +0530

    Upated log.py

--- log.py
+++ log.py
@@ -12,12 +12,16 @@ def start_logger():
     global logger
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
-    log_format = logging.Formatter('%(asctime)s|[%(levelname)8s]|[%(module)s.%(name)s.%(funcName)s]|%(lineno)4s|%(message)s')
-    log_path = Path(__file__).parent.joinpath('log')
+    log_format = logging.Formatter(
+        "%(asctime)s|[%(levelname)8s]|[%(module)s.%(name)s.%(funcName)s]|%(lineno)4s|%(message)s"
+    )
+    log_path = Path(__file__).parent.joinpath("log")
     if not os.path.isdir(log_path):
         os.makedirs(log_path)
-    file_name = log_path.joinpath('log_{}.log'.format(datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
-    file_handler = logging.FileHandler(filename=file_name, mode='a')
+    file_name = log_path.joinpath(
+        "log_{}.log".format(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    )
+    file_handler = logging.FileHandler(filename=file_name, mode="a")
     file_handler.setFormatter(log_format)
     logger.addHandler(file_handler)
 
@@ -29,12 +33,13 @@ def start_logger():
 def print_exception(e: object):
     exc_type, exc_obj, exc_tb = sys.exc_info()
     file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    short_message = 'Exception at {} - {} - {}\nCalling Function: {}'.format(exc_type,
-                                                                             file_name,
-                                                                             exc_tb.tb_lineno,
-                                                                             inspect.stack()[1][3])
-    message = '{}\n{}'.format(short_message, str(e))
+    short_message = (
+        f"Exception at {exc_type} - {file_name} - {exc_tb.tb_lineno}\n"
+        f"Calling Function: {inspect.stack()[1][3]}"
+    )
+    message = f"{short_message}\n{str(e)}"
     logger.debug(message)
+
 
 # Initialize Logger
 start_logger()
